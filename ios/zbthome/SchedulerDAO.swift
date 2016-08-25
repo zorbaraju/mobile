c1b66928ded1c750ca62fcfc1375162312ed95fc
raju s <uniraju@gmail.com>
2016-08-25 09:55:45 +0530

    repeat value

--- ios/zbthome/SchedulerDAO.swift
+++ ios/zbthome/SchedulerDAO.swift
@@ -12,19 +12,22 @@ class SchedulerDAO: NSObject, NSCoding {
 
     var name:String = ""
     var repeatType:String = ""
+    var repeatValue:String = ""
     var startTime:String = ""
     
     var devicesArray: [[Int]] = [[Int]]()
-    init(name:String, repeatType:String, startTime:String) {
+    init(name:String, repeatType:String, startTime:String, repeatValue: String) {
         self.name = name
         self.repeatType = repeatType
         self.startTime = startTime
+        self.repeatValue = repeatValue
         super.init()
     }
     
     required internal init(coder decoder: NSCoder) {
         name = decoder.decodeObjectForKey("name") as! String
         repeatType = decoder.decodeObjectForKey("repeatType") as! String
+        repeatValue = decoder.decodeObjectForKey("repeatValue") as! String
         startTime = decoder.decodeObjectForKey("startTime") as! String
         devicesArray = decoder.decodeObjectForKey("devicesArray") as! [[Int]]
     }
@@ -32,6 +35,7 @@ class SchedulerDAO: NSObject, NSCoding {
     internal func encodeWithCoder(coder: NSCoder) {
         coder.encodeObject(name, forKey: "name")
         coder.encodeObject(repeatType, forKey: "repeatType")
+        coder.encodeObject(repeatValue, forKey: "repeatValue")
         coder.encodeObject(startTime, forKey: "startTime")
         coder.encodeObject(devicesArray, forKey: "devicesArray")
     }
