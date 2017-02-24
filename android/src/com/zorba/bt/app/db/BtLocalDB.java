ff95fb2fad7d51ee9dce885a2673c468cf0bb94b
raju s <uniraju@gmail.com>
2017-02-24 12:29:18 +0530

    master switch

--- android/src/com/zorba/bt/app/db/BtLocalDB.java
+++ android/src/com/zorba/bt/app/db/BtLocalDB.java
@@ -381,6 +381,8 @@ public class BtLocalDB {
 	
 	public boolean isDeviceIdExist(String deviceName, int devid) {
 		String[] var5 = this.dbInfo.getString("Room" + deviceName, "").split("#");
+		if( (devid-1)*3 >= var5.length)
+			return false;
 		return !var5[(devid-1)*3].startsWith("Unknown ");
 	}
 
